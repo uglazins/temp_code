bb5ea220350d1027346ed0a3fba788f6a2088310
uglazins <64604403+uglazins@users.noreply.github.com>
2022-01-09 16:25:46 +0200

    Create useAuth.js

---
+++ hooks/useAuth.js
@@ -0,0 +1,91 @@
+import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signOut,
+} from "@firebase/auth";
+import { auth } from "../firebase";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useMemo } from "react";
+
+const AuthContext = createContext({});
+//Konfigurācija
+const config = {
+  androidClientId:
+    "",
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
+
+export const AuthProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loadingInitial, setLoadingInitial] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          //Pierakstījies
+          setUser(user);
+        } else {
+          //Nav pierakstījies
+          setUser(null);
+        }
+        setLoadingInitial(false);
+      }),
+    []
+  );
+//Izrakstīšanās funkcija
+  const logout = () => {
+    setLoading(true);
+
+    signOut(auth)
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+//Pierakstīšanās ar Google 
+  const signInWithGoogle = async () => {
+    setLoading(true);
+    await Google.logInAsync(config)
+      .then(async (logInResult) => {
+        if (logInResult.type === "success") {
+          const { idToken, accessToken } = logInResult;
+          const credential = GoogleAuthProvider.credential(
+            idToken,
+            accessToken
+          );
+          await signInWithCredential(auth, credential);
+        }
+
+        return Promise.reject();
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+//Saglabā , lai nebūtu jāpieprasa dati uz katru ielādi
+  const memoedValue = useMemo(
+    () => ({
+      user,
+      loading,
+      error,
+      signInWithGoogle,
+      logout,
+    }),
+    [user, loading, error]
+  );
+
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!loadingInitial && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
